1847eb4f034f6290fe3ccf8a9ff133a932fe777f
John Doe <julianosantosdev@outlook.com>
2023-07-24 00:04:23 -0300

    add: project files

---
+++ src/utils/stackData.ts
@@ -0,0 +1,57 @@
+import {
+  SiTypescript,
+  SiExpress,
+  SiPython,
+  SiTrello,
+  SiPostgresql,
+  SiHtml5,
+  SiCss3,
+  SiJavascript,
+  SiNodedotjs,
+  SiReact,
+  SiStyledcomponents,
+  SiChartdotjs,
+  SiLinuxmint,
+} from "react-icons/si";
+import { DiDjango } from "react-icons/di";
+import { FaGit } from "react-icons/fa";
+
+export const stackData = [
+  {
+    title: "HTML",
+    img: SiHtml5,
+  },
+  {
+    title: "CSS",
+    img: SiCss3,
+  },
+  {
+    title: "JS",
+    img: SiJavascript,
+  },
+  {
+    title: "Node JS",
+    img: SiNodedotjs,
+  },
+  {
+    title: "React",
+    img: SiReact,
+  },
+  {
+    title: "Styled Components",
+    img: SiStyledcomponents,
+  },
+  {
+    title: "Chart JS",
+    img: SiChartdotjs,
+  },
+
+  { title: "Express JS", img: SiExpress },
+  { title: "TypeScript", img: SiTypescript },
+  { title: "Django", img: DiDjango },
+  { title: "Git", img: FaGit },
+  { title: "Python", img: SiPython },
+  { title: "Postgres", img: SiPostgresql },
+  { title: "Trello", img: SiTrello },
+  { title: "Linux Mint", img: SiLinuxmint },
+];
---
+++ src/utils/userData.ts
@@ -0,0 +1,6 @@
+export const userData = {
+  nameUser: "Juliano Santos",
+  githubUser: "julianosantosdev",
+  linkedinUser: "juliano-santos-374100246",
+  whatsappNumber: "+5519996080304",
+};
